9227b3b947b99ad7683fa0309d2513a1475a8369
AFelipeCR <afeliperosas@gmail.com>
2026-06-23 17:27:06 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './openwa.module';
+export * from './services/base-http.service';
+export * from './services/sessions.service';
+export * from './services/webhooks.service';
+export * from './services/messages.service';
---
+++ src/openwa.module.ts
@@ -0,0 +1,58 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { BaseHttpService } from './services/base-http.service';
+import { MessagesService } from './services/messages.service';
+import { SessionsService } from './services/sessions.service';
+import { WebhooksService } from './services/webhooks.service';
+import { OpenWAConfig } from './interfaces/options.interface';
+
+@Global()
+@Module({})
+export class OpenWaModule {
+    static register(options: OpenWAConfig): DynamicModule {
+        return {
+            module: OpenWaModule,
+            providers: [
+                {
+                    provide: 'OPENWA_OPTIONS',
+                    useValue: options,
+                },
+                BaseHttpService,
+                SessionsService,
+                WebhooksService,
+                MessagesService
+            ],
+            exports: [
+                SessionsService, 
+                WebhooksService, 
+                MessagesService
+            ],
+        };
+    }
+
+    static registerAsync(
+        options: { 
+            inject?: any[]; 
+            useFactory: (...args: any[]) => Promise<OpenWAConfig> | OpenWAConfig;}
+    ): DynamicModule {
+        return {
+            module: OpenWaModule,
+            imports: [], 
+            providers: [
+                {
+                    provide: 'OPENWA_OPTIONS',
+                    useFactory: options.useFactory,
+                    inject: options.inject || [],
+                },
+                BaseHttpService,
+                SessionsService,
+                WebhooksService,
+                MessagesService
+            ],
+            exports: [
+                SessionsService, 
+                WebhooksService, 
+                MessagesService
+            ],
+        };
+    }
+}
